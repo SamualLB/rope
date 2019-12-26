d139abe4547a874ccdbdf8f7ffbacc0834dd3b81
Samual Black <samuallb@hotmail.com>
2019-12-26 17:04:03 +0000

    Printing, concatination and byte at

--- spec/rope_spec.cr
+++ spec/rope_spec.cr
@@ -4,4 +4,40 @@ describe Rope do
   it "works" do
     true.should eq(true)
   end
+
+
+  describe "#initialize" do
+    it "Can be created from a string" do
+      rope = Rope.new("test")
+      rope.should be_a(Rope)
+      rope.to_s.should eq "test"
+    end
+  end
+
+  describe "#byte_at" do
+    it "can find bytes" do
+      rope = Rope.new("123")
+      rope.byte_at(0).should eq '1'.ord
+      rope.byte_at(1).should eq '2'.ord
+      rope.byte_at(2).should eq '3'.ord
+    end
+  end
+
+  describe "#+" do
+    it "can concatinate 2 ropes" do
+      rope1 = Rope.new("test ")
+      rope2 = Rope.new("string")
+      joined = rope1 + rope2
+      joined.should be_a(Rope)
+      joined.to_s.should eq "test string"
+    end
+
+    it "can concatinate with a String" do
+      rope = Rope.new("test ")
+      str = "string"
+      joined = rope + str
+      joined.should be_a(Rope)
+      joined.to_s.should eq "test string"
+    end
+  end
 end
--- src/rope.cr
+++ src/rope.cr
@@ -1,3 +1,66 @@
 class Rope
   VERSION = "0.0.0"
+
+  getter! left : self
+  getter! right : self
+  getter! short : String
+  getter weight : Int32
+
+  protected setter left
+  protected setter right
+
+  def initialize(@short : String)
+    @left = nil
+    @right = nil
+    @weight = short.bytesize
+  end
+
+  protected def initialize(@weight)
+    @left = nil
+    @right = nil
+    @short = nil
+  end
+
+  def byte_at(index)
+    return right.byte_at(index - weight) if weight < index && right?
+    return left.byte_at(index) if left?
+    short.byte_at(index)
+  end
+
+  def byte_at?(index) 
+    return right.byte_at(index - weight) if weight < index && right?
+    return left.byte_at?(index) if left?
+    short.byte_at?(index)
+  end
+
+  def +(other : self) : Rope
+    root = Rope.new(bytesize)
+    root.left = self
+    root.right = other
+    root
+  end
+
+  def +(other : String) : Rope
+    root = Rope.new(bytesize)
+    root.left = self
+    root.right = Rope.new(other)
+    root
+  end
+
+  def bytesize : Int32
+    @short.try { |s| return s.bytesize }
+    sum = 0
+    @left.try { |l| sum += l.bytesize }
+    @right.try { |r| sum += r.bytesize }
+    return sum
+  end
+
+  def to_s(io : IO)
+    if short?
+      io << short
+      return
+    end
+    left.try { |l| l.to_s(io) }
+    right.try { |r| r.to_s(io) }
+  end
 end
